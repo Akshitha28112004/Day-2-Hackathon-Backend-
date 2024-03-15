e1a8e6751cf3b5fcffc703bae81c211e0dd84cb1
Akshitha28112004 <148344428+Akshitha28112004@users.noreply.github.com>
2024-03-15 19:48:50 +0530

    Book.js

---
+++ models/Book.js
@@ -0,0 +1,19 @@
+// models/Book.js
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  genre: String,
+  published: Number
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
